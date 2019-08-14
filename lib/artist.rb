e864f45744f4b9a7544330889c7e0fe5dbe11a4f
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-14 09:11:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,14 @@
 class Artist
-attr_accessor :name
+  @@all = []
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
 
   def self.all
-    @@all
+    @@all    
   end
 
   def add_song(song)
@@ -16,5 +17,21 @@ attr_accessor :name
 
   def songs
     Song.all.select {|song| song.artist == self}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.new(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+  def save
+    @@all << self
+  end
+
+  def print_songs
+    songs.each {|song| puts song.name}
   end
 end
